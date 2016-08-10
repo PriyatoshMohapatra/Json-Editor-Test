77f1e03f4dcda479748bcdad1a6fd1c8162ed036
Priyatosh Mohapatra <priyatosh@zool.in>
2016-08-10 19:09:11 +0530

    Test check

--- app/scripts/controllers/MainController.js
+++ app/scripts/controllers/MainController.js
@@ -1,4 +1,5 @@
 angular.module('clmMasterApp').controller('MainCtrl', ['$scope', function($scope) {
+    //Master Data 
     $scope.masterData = { "services": { "service": [{ "accountCode": "6608151", "serviceUser": { "profileDetails": { "basicDetails": { "dateOfBirth": { "$date": "1969-02-05T00:00:00.000Z" }, "lastName": "Kumar", "customerFullName": "Rajesh Kumar", "gender": { "masterCode": "M" }, "firstName": "Rajesh", "suffix": { "masterCode": "Sr" } } } }, "serviceDetails": { "serviceNumber": "22009364", "serviceStatus": { "masterCode": "ACT" }, "businessType": { "masterCode": "Postpaid" } }, "customerCode": "C596031", "serviceCode": "1358756" }] }, "billingAccounts": { "billingAccount": [{ "accountCode": "6608151", "customerCode": "C596031", "accountOwnerDetails": { "lastName": "Kumar", "customerFullName": "Kumar Rajesh", "firstName": "Rajesh" } }] } };
     $scope.configuration = {
         "editable": true,
